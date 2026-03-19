7fef41dfe5986adee9098812084e3009bd7e1658
Andy2887 <yuanliheng05@gmail.com>
2026-03-19 17:24:27 -0400

    chapter 3 done

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import tiktoken
 from torch.utils.data import Dataset, DataLoader
 
@@ -48,6 +49,74 @@ def create_dataloader_v1(txt, batch_size=4, max_length=256,
     )
 
     return dataloader
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        assert (d_out % num_heads == 0), \
+            "d_out must be divisible by num_heads"
+
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads # Reduce the projection dim to match desired output dim
+
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.out_proj = nn.Linear(d_out, d_out)  # Linear layer to combine head outputs
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer(
+            "mask",
+            torch.triu(torch.ones(context_length, context_length),
+                       diagonal=1)
+        )
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        # split into multiple heads
+        # (b, num_tokens, num_heads, head_dim)
+        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+        values = values.view(b, num_tokens, self.num_heads, self.head_dim)
+
+        # transpose weight matrices for subsequent operations
+        # (b, num_tokens, num_heads, head_dim) -> (b, num_heads, num_tokens, head_dim)
+        keys = keys.transpose(1, 2)
+        queries = queries.transpose(1, 2)
+        values = values.transpose(1, 2)
+
+        # calculate attention scores
+        # (b, num_heads, num_tokens, head_dim) @ (b, num_heads, head_dim, num_tokens)
+        # = (b, num_heads, num_tokens, num_tokens)
+        attn_scores = queries @ keys.transpose(2, 3)
+
+        # apply mask
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+        attn_scores.masked_fill_(mask_bool, -torch.inf)
+
+        # calculate attention weights
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+
+        # calculate context vec
+        # (b, num_heads, num_tokens, num_tokens) @ (b, num_heads, num_tokens, head_dim)
+        # = (b, num_heads, num_tokens, head_dim)
+        # then we transpose it
+        # (b, num_heads, num_tokens, head_dim) -> (b, num_tokens, num_heads, head_dim)
+        context_vec = (attn_weights @ values).transpose(1, 2)
+
+        # combine multiple heads together
+        context_vec = context_vec.contiguous().view(b, num_tokens, self.d_out)
+        # apply projection
+        context_vec = self.out_proj(context_vec)
+        
+        return context_vec
+
 
 
 
